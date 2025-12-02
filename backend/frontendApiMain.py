d275f90b498db71ed41f56f3fd724b1a46e52463
Ian Combe <icombe@mines.edu>
2025-12-01 18:58:12 -0700

    Critical CORS url fix

--- backend/frontendApiMain.py
+++ backend/frontendApiMain.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your React app's URL
+    allow_origins=["http://localhost:5173"],  # Your React app's URL
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
